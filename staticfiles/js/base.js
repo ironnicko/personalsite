70a85f4c00a4dc6532d54ddd037c9912cd60e9a4
ironnicko <nickonicko779@gmail.com>
2021-12-02 02:57:08 +0530

    staticfiles added

---
+++ staticfiles/js/base.js
@@ -0,0 +1,30 @@
+$(document).ready(
+    () => $('#loading_wrap').remove()
+)
+var prev;
+function openNav() {
+    $(".btn").css(
+        {
+            "z-index":"-1"
+        }
+    )
+    document.getElementById("mySidenav").style.width = "20rem";
+    document.getElementById("main").style.marginLeft = "20rem";
+    prev = window.getComputedStyle(document.body).getPropertyValue("background-color")
+    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+}
+
+function closeNav() {
+    $(".btn").css(
+        {
+            "z-index":"1"
+        }
+    )
+    document.getElementById("mySidenav").style.width = "0";
+    document.getElementById("main").style.marginLeft = "0";
+    document.body.style.backgroundColor = prev;
+}
+
+var page = document.URL.split("/").slice(-1)[0] || "home";
+
+$("#mLink").html(`${page.toUpperCase()}`)
