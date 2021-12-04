20501b1f92a132b86f98d5be410884ae13e15d9d
ironnicko <nickonicko779@gmail.com>
2021-12-05 01:04:24 +0530

    changes done to the footer

--- static/js/base.js
+++ static/js/base.js
@@ -18,7 +18,7 @@ function openNav() {
     document.getElementById("mySidenav").style.width = "20rem";
     document.getElementById("main").style.marginLeft = "20rem";
     prev = window.getComputedStyle(document.body).getPropertyValue("background-color")
-    document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
+    // document.body.style.backgroundColor = "rgba(0,0,0,0.4)";
 }
 
 function closeNav() {
