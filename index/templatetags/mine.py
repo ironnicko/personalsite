a35fb876dbe84a6c4e6e1191cc3578f8ea9538cd
ironnicko <nickonicko779@gmail.com>
2021-12-08 17:56:55 +0530

    Removed carousel and other animations added

--- index/templatetags/mine.py
+++ index/templatetags/mine.py
@@ -6,6 +6,9 @@ def get(value, arg):
     return value.get(arg)
 def index(value, arg):
     return value[arg]
+def rem(value, arg):
+    return value%arg
 
 register.filter("get", get)
+register.filter("rem", rem)
 register.filter("index", index)
--- index/views.py
+++ index/views.py
@@ -2,7 +2,7 @@ from django.shortcuts import render
 from django.contrib import messages
 from .forms import PersonForm
 from .models import Person
-from json import dumps
+from json import dump, dumps
 
 # Create your views here.
 def contact(request):
@@ -22,10 +22,13 @@ def contact(request):
     return render(request, "contact.html", form)
 
 def home(request):
-    main = [("d2socials.png", "A site made to get my classmates socials out to everyone our class."), ("online-class-launcher.png", "A program made to open my online-class links on time."), ("marine-derbis.jpeg", "Project work done on marine debris for NASA Science Apps 2021 competition.")]
+    main = [("d2socials.png", "A site made to get my classmates socials out to everyone in our class."), ("iphone_classifier.jpeg", "A project I made while preparing for NASA SpaceApps Challenge 2021. The program does what it says: detects iPhoneXR."),("online-class-launcher.png", "A program made to open my online-class links on time."), ("marine-derbis.jpeg", "Project work done on marine debris for NASA Science Apps 2021 competition.")]
+    logos = ["python.svg", "postgresql.svg"]
     name = {i[0].split(".")[0]:(i[0], i[1]) for i in main}
     if request.method == "GET":
         form = {
             "name" : name,
+            "nameJS" : dumps(name),
+            "logos" : logos
             }
         return render(request, "home.html", context=form)
