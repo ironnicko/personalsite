476fe97f33a312899c3c64903c3841ca32a4e456
ironnicko <nickonicko779@gmail.com>
2021-12-08 17:57:37 +0530

    Removed carousel and other animations added

--- staticfiles/js/home.js
+++ staticfiles/js/home.js
@@ -1,12 +1,17 @@
-$("#carlink").on("mouseenter mouseleave", e=>{
-    $(".lead").toggle()
-    $(".car-head").toggle()
-})
+// $(".carousel-caption").css("backdrop-filter", "blur(0.1rem)")
+// $("#carlink").on("mouseenter mouseleave", e=>{
+//     if (e.type == "mouseenter"){
+//         gsap.to(".carousel-caption", 0.5, {opacity: 0})
+//     }
+//     else{
+//         gsap.to(".carousel-caption", 0.5, {opacity: 1})
+//     }
+// })
 
 //Animation
 gsap.registerPlugin(ScrollTrigger);
 
-var tlm = gsap.timeline({delay:1});
+var tlm = gsap.timeline();
 tlm
 .to("#welcomeText", {
     duration: 1.5,
@@ -26,11 +31,21 @@ tlm
     repeat: -1,
     scale: 1.1,
     yoyo: true,
-    pin: true,
     ease: "sine.inOut",
     scrollTrigger: {
         trigger: "#triangeSVGdiv",
         start: "top bottom",
+        toggleActions: "play pause play pause"
+    }
+})
+.from(".card", {
+    duration: 0.1,
+    opacity: 0,
+    x: 1000,
+    ease: "cos.inOut",
+    scrollTrigger: {
+        trigger: "#projectDiv",
+        start: "top top",
         toggleActions: "play pause play pause"
     }
 })
@@ -45,13 +60,13 @@ tlm
         toggleActions: "play pause play pause"
     }
 })
-gsap.from("#projectDiv",{
-    duration: 1,
-    x : -1000,
-    ease: "sine.inOut",
-    scrollTrigger: {
-        start: "top bottom",
-        trigger: "#projectDiv",
-        toggleActions: "play pause play pause"
-    }
-})
+// gsap.from("#projectDiv",{
+//     duration: 1,
+//     x : -1000,
+//     ease: "sine.inOut",
+//     scrollTrigger: {
+//         start: "top bottom",
+//         trigger: "#projectDiv",
+//         toggleActions: "play pause play pause"
+//     }
+// })
