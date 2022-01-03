402b27babab4cefb105dc3c1d8ac92675f652775
ironnicko <nickonicko779@gmail.com>
2022-01-04 05:11:17 +0530

    added environment variable

--- personal/settings.py
+++ personal/settings.py
@@ -22,7 +22,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # See https://docs.djangoproject.com/en/3.2/howto/deployment/checklist/
 
 # SECURITY WARNING: keep the secret key used in production secret!
-SECRET_KEY = "django-insecure-&4w74e@rgc)4nyjf+x*@e_kqwb%sb*s1lc=)7yu(#=1+qam=f2"
+SECRET_KEY = os.getenv("SECRET_KEY")
 # CSRF_COOKIE_SECURE = True
 # CSRF_COOKIE_HTTPONLY = True
 
@@ -154,6 +154,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 EMAIL_HOST = "smtp.gmail.com"
 EMAIL_PORT = "587"
-EMAIL_HOST_USER = "nikhilivannancontact@gmail.com"
-EMAIL_HOST_PASSWORD = "Hussu6-pakbyv-quvmib"
+EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
+EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
 EMAIL_USE_TLS = True
