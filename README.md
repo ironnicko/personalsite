882577ec9581c8c62f37093afa8ff480f2579ef7
ironnicko <nickonicko779@gmail.com>
2022-01-15 03:22:44 +0530

    Merge branch 'main' of https://github.com/ironnicko/personalsite

--- README.md
+++ README.md
@@ -1,7 +1,1 @@
-# Welcome
-
-# <a href="http://nikhil-i.link">Link</a>
-
-This is my site made using Django, PostgresQL, CSS, Javascript, GSAP, and JQuery.
-
-This is a site in continuous development, so will be worked on regularly- if I don't have freaking assignments!!
+# In-progress
--- staticfiles/js/home.js
+++ staticfiles/js/home.js
@@ -11,7 +11,7 @@ var titles = [
 var tlm = gsap.timeline();
 tlm
 .from("#welcomeText", {
-    duration: 0.5,
+    duration: 2,
     x: "-=500",
     yoyo: true,
     opacity: 0, 
@@ -19,8 +19,8 @@ tlm
     scrollTrigger: {
         scrub : 1,
         trigger: "#welcomeText",
-        start: "bottom bottom",
-        end: "top top",
+        start: "center center",
+        end: "bottom bottom",
         toggleActions: "play pause play pause"
     }
 })
@@ -38,20 +38,7 @@ tlm
         toggleActions: "play pause play pause"
     }
 })
-// .from("#cardProjects", {
-//     duration: 1.5,
-//     opacity: 0,
-//     x: 500,
-//     autoAlpha: 1,
-//     ease: "sine.out",
-//     scrollTrigger: {
-//         scrub: 1,
-//         trigger: "#second .col",
-//         start: "top bottom",
-//         end: "bottom top", 
-//         toggleActions: "play pause play pause", 
-//     }
-// })
+
 titles.forEach(item => {
     tlm.from(item, {
     duration: 1.5,
@@ -59,10 +46,9 @@ titles.forEach(item => {
     x: 500,
     ease: "sine.out",
     scrollTrigger: {
-        scrub: 0.75,
         trigger: item,
-        start: "top top",
-        end: "bottom bottom", 
+        start: "top 200px top",
+        end: "center bottom", 
         toggleActions: "restart complete complete reverse", 
     }}
     )
@@ -86,7 +72,7 @@ gsap.from("#skillsCard", {
     x: 500,
     ease: "power2.out",
     scrollTrigger: {
-        trigger: "#aboutmeContainer",
+        trigger: "#skillsCard",
         start: "left right",
         end: "bottom bottom",
         toggleActions: "restart complete complete reverse"
